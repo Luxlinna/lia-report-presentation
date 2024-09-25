ed5a64ce83d2c650e409dd58cb46466db8a919dd
LochLinna <ylinna168@gmail.com>
2024-09-25 18:12:25 +0700

    update TableOfContent

--- src/components/TableOfContents.jsx
+++ src/components/TableOfContents.jsx
@@ -5,9 +5,9 @@ import { Link } from 'react-router-dom';
 const TableOfContents = () => {
   return (
     <div className="custom-container flex flex-col items-center justify-center h-screen bg-gray-100">
-      <div className='content-container w-full'>
+      <div className='content-container w-full flex flex-col justify-center'>
         <h1 className="text-4xl font-bold mb-8 text-green-900">Table of Contents</h1>
-        <ul className="text-lg space-y-4">
+        <ul className="text-lg space-y-4 pl-12">
           <li><Link to="/summary" className="text-blue-600 hover:underline">1. Summary</Link></li>
           <li><Link to="/introduction" className="text-blue-600 hover:underline">2. Introduction</Link></li>
           <li><Link to="/purpose-goals" className="text-blue-600 hover:underline">3. Purpose, Goals, and Problem Statement</Link></li>
